b1711f54632b93f13c06fc5d9b70e3539a8b8e58
Duarte Cota <duartecota@MacBook-Air-de-Duarte-6.local>
2019-02-07 12:44:10 -0100

    7 fev

--- comando_BT_cota/comando_BT_cota.ino
+++ comando_BT_cota/comando_BT_cota.ino
@@ -1,34 +1,52 @@
-#include <AlphaBot.h>
-
 #include <BnrOneA.h>
 #include <EEPROM.h>
 #include <SPI.h>
+#include <SoftwareSerial.h>
 
 BnrOneA one;
+
+SoftwareSerial HC06(0,1); //RX, TX
 
 #define led 5
 #define SSPIN 2
 
-boolean validaDados (String dados);
+boolean validaDados(String dados);
+boolean readObstSensors();
+char getComando(String dados);
+int getVelocidade(String dados);
+
 
 void setup() {
 
   pinMode(led, OUTPUT);
   Serial.begin(9600);
+  HC06.begin(9600);
   one.spiConnect(SSPIN);
+  one.obstacleEmitters(ON);
+  one.lcd1("ENTA#CPROB");
   one.stop();
 }
 
 void loop() 
 {
   String dados = "";
-  while (Serial.available()) {
+  while (HC06.available()) {
     dados = Serial.readString();
-    Serial.print("String recebida:\n");
+    HC06.print("String recebida:");
+    HC06.println(dados);
+    one.lcd1("ENTA#CPROB");
+    one.lcd2(dados);
   }
-  if(validaDados(dados))
+  if(dados!="")
   {
-    Serial.println("String validada");
+    if(validaDados(dados)==true)
+    {
+      HC06.println("String validada!");
+      HC06.println(getComando(dados));
+      HC06.println(getVelocidade(dados));
+    }
+    else 
+      HC06.println("String invalida!");
   }
   
   /*String comando = (String) dados.charAt(0) + (String) dados.charAt(1);
@@ -87,21 +105,46 @@ void loop()
 /****************************************************************/
 boolean validaDados(String dados)
 {
-  boolean valida=false;
+  int valida=false;
   char comandos[6] = {'F','B','S','L','R'};
-  char aux[5] = {'+','-',' ','\n'};
   for (int i=0;i<5;i++)
   {
     if(dados.charAt(0)==comandos[i] && dados)
-    {
-      for(int j=0;j<4;j++)
-      {
-        if(dados.charAt(1)==aux[j])
-          valida=true;
-      }
-    }
-    else
-      valida=false;
+      valida=true;
   }
   return valida;
+}
+
+char getComando(String dados)
+{
+  return dados.charAt(0);
+}
+
+int getVelocidade(String dados)
+{
+  String velStr;
+  for (int i=1;i<dados.length();i++)
+    velStr += dados.charAt(i);
+  return velStr.toInt();
+}
+
+boolean readObstSensors()
+{
+  byte sensores = one.obstacleSensors();
+  //boolean obstaculo = false;
+  switch(sensores)
+  {
+    case 0:
+      return false;
+      break;
+    case 1:
+      return true;
+      break;
+    case 2:
+      return true;
+      break;
+    case 3:
+      return true;
+      break;
+  }
 }
