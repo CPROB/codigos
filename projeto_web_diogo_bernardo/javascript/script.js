5d6bcf3e3f758c842210ad4a101753ba015f2cbc
duarte cota <cprob@enta.pt>
2020-10-16 17:39:17 +0000

    alteração inicial

---
+++ projeto_web_diogo_bernardo/javascript/script.js
@@ -0,0 +1,8 @@
+function converter(){
+    var array = [];
+    array.push(3);
+    array.push(6);
+    array.push(9);
+    document.getElementById('output_numero').innerHTML = array[1];
+    document.getElementById('teste').innerHTML = "Texto na div";
+}
